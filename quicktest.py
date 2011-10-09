f251e45c190dc672bdb43de732e42c12bb35f623
Marius Gedminas <marius@gedmin.as>
2011-10-09 12:53:25 +0200

    Python script for quick QML prototyping on the device.

---
+++ quicktest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os, sys, signal
+from PySide import QtGui, QtDeclarative
+
+here = os.path.dirname(__file__)
+qml_file = os.path.join(here, 'qml/TimeTracker/main.qml')
+
+app = QtGui.QApplication(sys.argv)
+view = QtDeclarative.QDeclarativeView(qml_file)
+view.showFullScreen()
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal.SIG_DFL) # otherwise PySide ignores ^C
+    app.exec_()
