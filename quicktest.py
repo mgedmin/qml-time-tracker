5e5afe64e1cf09e57de7647231f2a83191c5d06d
Marius Gedminas <marius@gedmin.as>
2011-10-12 14:08:05 +0300

    Don't launch the UI when QML fails to load.

--- quicktest.py
+++ quicktest.py
@@ -7,8 +7,12 @@ qml_file = os.path.join(here, 'qml/TimeTracker/main.qml')
 
 app = QtGui.QApplication(sys.argv)
 view = QtDeclarative.QDeclarativeView(qml_file)
+
 view.showFullScreen()
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL) # otherwise PySide ignores ^C
-    app.exec_()
+    if view.status == view.Ready:
+        app.exec_()
+    else:
+        print "Could not load %s, exitting" % qml_file
